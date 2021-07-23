6e2afa2243350e6929ce19bebcddef099c081ab0
Yamamotonasu <deep.blue.deep@outlook.jp>
2021-07-23 15:58:19 +0900

    fix unnecessary import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:snappy/themes/theme.dart';
 
