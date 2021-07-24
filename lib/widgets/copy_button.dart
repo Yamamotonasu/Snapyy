92834d9543ad595d368d4ed621e9d4bf43f372c0
Yamamotonasu <deep.blue.deep@outlook.jp>
2021-07-24 20:28:21 +0900

    Add snackbar when copyed

--- lib/widgets/copy_button.dart
+++ lib/widgets/copy_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:snappy/utils/localization.dart';
 
 class CopyButton extends StatelessWidget {
 
@@ -7,15 +8,26 @@ class CopyButton extends StatelessWidget {
 
   CopyButton({required this.copyText});
 
-  Future<void> onTapCopyButton({required String copyText}) async {
+  Future<void> onTapCopyButton({required String copyText, required BuildContext context}) async {
     final copyData = ClipboardData(text: copyText);
     await Clipboard.setData(copyData);
+    final copyedText = AppLocalizations.instance.text('copyed');
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("$copyText $copyedText", style: Theme.of(context).textTheme.headline4),
+        duration: const Duration(seconds: 3),
+        action: SnackBarAction(
+        label: AppLocalizations.instance.text('confirm'),
+        onPressed: () {},
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: Feedback.wrapForTap(() => onTapCopyButton(copyText: copyText), context),
+      onTap: Feedback.wrapForTap(() => onTapCopyButton(copyText: copyText, context: context), context),
       child: Image(
         image: AssetImage('lib/assets/copy_icon.png'),
         width: 30,
